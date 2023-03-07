7436679fa97f8ea39aba0b3e7885bab3fb8bd48b
Benjamin Schnabel <Benjamin-777@gmx.de>
2023-03-07 14:57:19 +0100

    Wagtail 4.1.2 added.

---
+++ haskala/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-h_^ko*b=)ayhfhdus(%$=frf8hexku0kzuhpxxfe_ejg11)@nz"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
